405e7d20edd3cf51c6a6ac541955ecf295fae276
kaifransson <franssonkai@gmail.com>
2017-07-27 17:32:56 +0200

    properly read stuff into the MedsConfig

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,57 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
 
 import           Control.Monad
-import           Control.Monad.Trans.Except
+import           Control.Monad.Except
+import           Control.Monad.Trans
 import           Control.Monad.Trans.Reader
 import           Data.Dates
+import           Data.Ini
+import           Data.Text
 import           Lib
+
+configPath :: FilePath
+configPath = "app\\config.ini"
+
+configg = pack "CONFIG"
+
+startMedDay = pack "StartMedDay"
+
+startYear = pack "StartYear"
+startMonth = pack "StartMonth"
+startDay = pack "StartDay"
+
+startDate = pack "StartDate"
+
+readIni :: IO Ini
+readIni = do
+    (Right ini) <- readIniFile configPath
+    return ini
+
+liftEither :: ( Monad m
+              , MonadError e m )
+              => Either e r -> m r
+liftEither = either throwError return
+
+loadConfig :: ( MonadIO m
+              , MonadError String m )
+              => m MedsConfig
+loadConfig = do
+    ini <- liftIO readIni
+    let get key = liftEither $ read . unpack <$> lookupValue configg key ini
+    sy <- get startYear
+    sm <- get startMonth
+    sd <- get startDay
+    let sdt = DateTime sy sm sd 0 0 0
+    smd <- get startMedDay
+    today <- liftIO getCurrentDateTime
+    return $ Config sdt today smd
 
 main :: IO ()
 main = do
-    today <- getCurrentDateTime
-    let config = Config undefined today
+    (Right config) <- runExceptT loadConfig
     result <- runExceptT $ runReaderT currentMedDay config
     either oops yay result
     void getLine
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,47 +10,28 @@ import           Control.Monad.Error.Class
 import           Control.Monad.Reader.Class
 import           Control.Monad.Trans
 import           Data.Dates
-import           Data.Ini
-import           Data.Text
 
 data MedDay = Arms
             | Legs
             deriving (Eq, Show, Read)
 
 data MedsConfig = Config { getStartDate   :: DateTime
-                         , getCurrentDate :: DateTime }
+                         , getCurrentDate :: DateTime
+                         , getStartMedDay :: MedDay }
 
-configPath :: FilePath
-configPath = "app\\config.ini"
-
-config = pack "CONFIG"
-
-startMedDay = pack "StartMedDay"
-
-startYear = pack "StartYear"
-startMonth = pack "StartMonth"
-startDay = pack "StartDay"
-
-startDate = pack "StartDate"
-
-calcShit :: Ini -> DateTime -> Either String MedDay
-calcShit ini today = do
-    let get val = read . unpack <$> lookupValue config val ini
-    smd <- get startMedDay
-    sy <- get startYear
-    sm <- get startMonth
-    sd <- get startDay
-    let startDay = DateTime sy sm sd 0 0 0
-    let daysPassed = datesDifference today startDay
-    return $ if even daysPassed then smd else flop smd
-    where flop Arms = Legs
-          flop Legs = Arms
+calcShit :: MedsConfig -> MedDay
+calcShit config = let startDay = getStartDate config
+                      today = getCurrentDate config
+                      smd = getStartMedDay config
+                      daysPassed = datesDifference today startDay
+                   in if even daysPassed then smd else flop smd
+                where flop Arms = Legs
+                      flop Legs = Arms
 
 currentMedDay :: ( MonadIO m
                  , MonadError String m
                  , MonadReader MedsConfig m )
                  => m MedDay
 currentMedDay = do
-    today <- getCurrentDate <$> ask
-    (Right ini) <- liftIO $ readIniFile configPath
-    either throwError return (calcShit ini today)
+    config <- ask
+    return $ calcShit config
