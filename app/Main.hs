5bf128e67c1e942bb735183d9ef2b1c1a016b9c4
Sara Fransson <sara.fransson@klarna.com>
2021-06-17 13:06:34 +0200

    move main out to the exe target

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
 module Main
   ( main
   ) where
 
-import qualified Meds.Main as Meds
+import           Data.Aeson                 (eitherDecodeFileStrict, encode)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import           Data.Functor               (void)
+import           Meds                       (currentMedDay)
+import           Meds.App                   (runMedsAppT)
+import           Meds.Calendar              (runIOCalendarT)
+import           Meds.Config                (MedsConfig)
+import           Paths_Meds                 (getDataFileName)
+import           System.Exit                (exitFailure)
+
+loadConfig :: IO (Either String MedsConfig)
+loadConfig =
+  getDataFileName "config.json" >>= eitherDecodeFileStrict
 
 main :: IO ()
-main = Meds.main
+main = do
+  config <- loadConfig >>= \case
+    Left err -> do
+      putStrLn "Failed to deserialize config"
+      putStrLn err
+      exitFailure
+    Right cfg -> pure cfg
+
+  (runIOCalendarT . runMedsAppT config) currentMedDay >>= LBS.putStrLn . encode
+  void getLine
